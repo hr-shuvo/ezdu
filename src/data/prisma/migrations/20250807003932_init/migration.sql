e5c03f7428226684200dcbb795b19b36a5e1fc87
hr-shuvo <h.r.shuvo9@gmail.com>
2025-08-07 06:41:50 +0600

    update models

---
+++ src/data/prisma/migrations/20250807003932_init/migration.sql
@@ -0,0 +1,239 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `role` ENUM('SUPER_ADMIN', 'ADMIN', 'MODERATOR', 'USER') NOT NULL DEFAULT 'USER',
+    `isVerified` BOOLEAN NOT NULL DEFAULT false,
+    `hasNotifications` BOOLEAN NOT NULL DEFAULT false,
+    `avatar` VARCHAR(191) NULL,
+    `avatarPublicId` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    INDEX `User_username_idx`(`username`),
+    INDEX `User_email_idx`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserAuthToken` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `verifyToken` VARCHAR(255) NOT NULL,
+    `resetToken` VARCHAR(255) NULL,
+    `loginToken` VARCHAR(255) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `expiresAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `UserAuthToken_userId_key`(`userId`),
+    UNIQUE INDEX `UserAuthToken_verifyToken_key`(`verifyToken`),
+    UNIQUE INDEX `UserAuthToken_resetToken_key`(`resetToken`),
+    UNIQUE INDEX `UserAuthToken_loginToken_key`(`loginToken`),
+    INDEX `UserAuthToken_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Notification` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(50) NOT NULL,
+    `message` TEXT NOT NULL,
+    `isRead` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `Notification_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `UserProgress` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `userAvatar` VARCHAR(191) NULL,
+    `xp` DOUBLE NOT NULL DEFAULT 0,
+    `level` INTEGER NOT NULL DEFAULT 1,
+    `rank` INTEGER NOT NULL DEFAULT 0,
+    `totalQuizzes` INTEGER NOT NULL DEFAULT 0,
+    `lastStreakDate` DATETIME(3) NULL,
+    `currentStreak` INTEGER NOT NULL DEFAULT 0,
+    `longestStreak` INTEGER NOT NULL DEFAULT 0,
+    `badges` VARCHAR(191) NULL,
+    `weeklyXp` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `UserProgress_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LearningProgress` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AcademyClass` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(100) NOT NULL,
+    `title` VARCHAR(250) NOT NULL,
+    `description` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `segment` ENUM('JUNIOR', 'SSC', 'HSC', 'ADMISSION', 'JOB') NOT NULL,
+    `groups` VARCHAR(191) NULL,
+    `hasBatch` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `AcademyClass_slug_key`(`slug`),
+    INDEX `AcademyClass_slug_idx`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Subject` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(100) NOT NULL,
+    `title` VARCHAR(250) NOT NULL,
+    `description` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `hasSubjectPaper` BOOLEAN NOT NULL DEFAULT false,
+    `subjectId` VARCHAR(191) NULL,
+    `classId` VARCHAR(191) NOT NULL,
+    `groups` VARCHAR(191) NULL,
+    `segment` ENUM('JUNIOR', 'SSC', 'HSC', 'ADMISSION', 'JOB') NULL,
+
+    UNIQUE INDEX `Subject_slug_key`(`slug`),
+    UNIQUE INDEX `Subject_classId_key`(`classId`),
+    INDEX `Subject_slug_idx`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Lesson` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(100) NOT NULL,
+    `title` VARCHAR(250) NOT NULL,
+    `description` TEXT NULL,
+    `order` INTEGER NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `subjectId` VARCHAR(191) NOT NULL,
+    `lessonId` VARCHAR(191) NULL,
+    `classId` VARCHAR(191) NULL,
+    `segment` ENUM('JUNIOR', 'SSC', 'HSC', 'ADMISSION', 'JOB') NULL,
+
+    UNIQUE INDEX `Lesson_slug_key`(`slug`),
+    INDEX `Lesson_slug_id_idx`(`slug`, `id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LessonContent` (
+    `id` VARCHAR(191) NOT NULL,
+    `lessonId` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(250) NOT NULL,
+    `description` TEXT NULL,
+    `contentType` ENUM('VIDEO', 'PDF', 'IMAGE', 'TEXT') NOT NULL DEFAULT 'TEXT',
+    `contentFileUrl` TEXT NULL,
+    `order` INTEGER NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `LessonContent_lessonId_id_idx`(`lessonId`, `id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Mcq` (
+    `id` VARCHAR(191) NOT NULL,
+    `question` VARCHAR(255) NOT NULL,
+    `options` TEXT NOT NULL,
+    `subjectId` VARCHAR(191) NOT NULL,
+    `lessonId` VARCHAR(191) NULL,
+    `topicId` VARCHAR(191) NULL,
+    `passage` VARCHAR(191) NULL,
+    `imageUrl` VARCHAR(255) NULL,
+    `imagePublicId` VARCHAR(255) NULL,
+    `description` VARCHAR(255) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `institutes` VARCHAR(191) NULL,
+
+    INDEX `Mcq_id_lessonId_topicId_subjectId_idx`(`id`, `lessonId`, `topicId`, `subjectId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Quiz` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `duration` INTEGER NOT NULL,
+    `score` DOUBLE NULL,
+    `startedAt` DATETIME(3) NOT NULL,
+    `completedAt` DATETIME(3) NULL,
+    `subjectId` VARCHAR(191) NOT NULL,
+    `questions` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `isSubmitted` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AdmissionCategory` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(100) NOT NULL,
+    `title` VARCHAR(250) NOT NULL,
+    `description` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `AdmissionCategory_slug_key`(`slug`),
+    INDEX `AdmissionCategory_slug_idx`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AdmissionUnit` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(100) NOT NULL,
+    `title` VARCHAR(250) NOT NULL,
+    `description` VARCHAR(250) NULL,
+
+    UNIQUE INDEX `AdmissionUnit_slug_key`(`slug`),
+    INDEX `AdmissionUnit_slug_idx`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AdmissionSubject` (
+    `id` VARCHAR(191) NOT NULL,
+    `subjectId` VARCHAR(191) NOT NULL,
+    `categoryId` VARCHAR(191) NULL,
+    `unitId` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `AdmissionSubject_subjectId_categoryId_unitId_key`(`subjectId`, `categoryId`, `unitId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `AdmissionSubject` ADD CONSTRAINT `AdmissionSubject_subjectId_fkey` FOREIGN KEY (`subjectId`) REFERENCES `Subject`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AdmissionSubject` ADD CONSTRAINT `AdmissionSubject_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `AdmissionCategory`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AdmissionSubject` ADD CONSTRAINT `AdmissionSubject_unitId_fkey` FOREIGN KEY (`unitId`) REFERENCES `AdmissionUnit`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/types/academy.types.ts
@@ -0,0 +1,8 @@
+
+enum AcademyGroup{
+    SCIENCE = 'SCIENCE',
+    ARTS = 'ARTS',
+    COMMERCE = 'COMMERCE',
+}
+
+export const ACADEMY_GROUPS = Object.values(AcademyGroup);
